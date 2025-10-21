03dd7ef46dfca1074ff83273abc0e961085f76ac
Joaquin Alegre <joaco12-2002@hotmail.com>
2025-10-21 17:32:36 -0300

    le agregue un home e hice una linda interfaz en el para el usuario, con una imagen de fondo y un nav lindo

--- src/app/service/torneo-service/torneo-service.ts
+++ src/app/service/torneo-service/torneo-service.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import Torneo from '../../model/torneo';
 
-@Component({
-  selector: 'app-torneo-service',
-  imports: [],
-  templateUrl: './torneo-service.html',
-  styleUrl: './torneo-service.css'
+@Injectable({
+  providedIn: 'root'
 })
-export class TorneoService {
+
+export class torneoService{
+
+      private url = 'http://localhost:3000/torneos'; 
+    private nextId   = 1;
+
+  constructor(private http: HttpClient) {}
+
+  getTorneo(): Observable<Torneo[]> {
+    return this.http.get<Torneo[]>(this.url);
+  }
+
+  getTorneoById(id: number): Observable<Torneo> {
+    return this.http.get<Torneo>(`${this.url}/${id}`);
+  }
+
+
+  postTorneo(torneo: Torneo): Observable<Torneo> {
+    //torneo.id = this.nextId++;
+    return this.http.post<Torneo>(this.url, torneo);
+  }
+
+  updateTorneo(torneo: Torneo): Observable<Torneo> {
+    return this.http.put<Torneo>(`${this.url}/${torneo.id}`, torneo);
+  }
+
+  deleteTorneo(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.url}/${id}`);
+  }
 
 }
