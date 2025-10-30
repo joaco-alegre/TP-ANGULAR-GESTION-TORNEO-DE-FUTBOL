6f799e1450d5a45fff63a9f7fb55680fb659ef0a
Joaquin Alegre <joaco12-2002@hotmail.com>
2025-10-30 18:50:32 -0300

    agregue las funciones necesarias del administrador para el crud de torneo y jugadores

--- Gestion-torneo-futbol-front/src/app/pages/jugador/jugador-form/jugador-form.ts
+++ Gestion-torneo-futbol-front/src/app/pages/jugador/jugador-form/jugador-form.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-jugador-form',
@@ -6,6 +6,54 @@ import { Component } from '@angular/core';
   templateUrl: './jugador-form.html',
   styleUrl: './jugador-form.css'
 })
-export class JugadorForm {
+export class JugadorForm implements OnInit{
+
+  
+  torneoForm!: FormGroup;
+  torneoID?: number;
+
+  constructor(
+    private fb: FormBuilder,
+    private torneoService: TorneoService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.torneoForm = this.fb.group({
+      id: ['', [Validators.required]],
+      nombre: ['', [Validators.required]],
+      nombreTorneo: ['', [Validators.required]],
+      DT: ['', [Validators.required]]
+    });
+/*
+    this.equipoID = this.route.snapshot.params['id'];
+    if (this.equipoID) {
+      this.equipoService.getEquipoById(this.equipoID)
+        .subscribe(equipo => this.equipoForm.patchValue(equipo));
+    }*/
+  }
+
+  onSubmit(): void {
+  
+    if (this.torneoForm.invalid) return;
+
+    if (this.torneoForm) {
+
+      const torneoData: Torneo = { id: this.torneoID, ...this.torneoForm.value }; 
+
+      this.torneoService.updateTorneo(torneoData).subscribe({
+        next: () => this.router.navigate(['/es/torneos']),
+        error: (e) => {console.log(e)} });
+
+    } else {
+
+      const torneoData = this.torneoForm;
+      
+      this.torneoService.postTorneo(torneoData).subscribe({
+        next: () => this.router.navigate(['/es/torneos']),
+        error: (e) => {console.log(e)} });
+    }
+  }
 
 }
--- Gestion-torneo-futbol-front/src/app/pages/usuario/usuario-home/usuario-home.ts
+++ Gestion-torneo-futbol-front/src/app/pages/usuario/usuario-home/usuario-home.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import Usuario from '../../../model/usuario';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-usuario-home',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './usuario-home.html',
   styleUrl: './usuario-home.css',
 })
 export class UsuarioHome {
+
+  usuario!: Usuario;
 
 }
